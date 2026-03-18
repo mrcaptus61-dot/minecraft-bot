3beb0cba18fbe1891bdfb4d527aeac070e1b1f27
mrcaptus61-dot <mrcaptus61@gmail.com>
2026-03-18 22:10:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 const mineflayer = require('mineflayer')
-const http = require('http')
 
-http.createServer((req, res) => {
-  res.writeHead(200)
-  res.end('Bot calisiyor!')
-}).listen(3000)
+const bot = mineflayer.createBot({
+  host: 'samsun5361.aternos.me',
+  port: 21808,
+  username: 'AfkBot_Mert',
+  version: '1.21.1',
+  checkTimeoutInterval: 60000
+})
 
-function createBot() {
-  const bot = mineflayer.createBot({
-    host: 'samsun5361.aternos.me',
-    port: 21808,
-    username: 'AfkBot_Mert',
-    version: '1.21.1',
-    checkTimeoutInterval: 60000,
-    connect: (client) => {
-      client.socket.setKeepAlive(true, 4000)
-    }
-  })
+bot.on('spawn', () => {
+  console.log('Bot sunucuya baglandi!')
+})
 
-  bot.on('spawn', () => {
-    console.log('Bot sunucuya baglandi!')
-  })
+bot.on('end', (reason) => {
+  console.log('Baglanti kesildi:', reason)
+  // GitHub Actions timeout ile yeniden tetiklenecek
+})
 
-  bot.on('end', (reason) => {
-    console.log('Baglanti kesildi (' + reason + '), 10 saniye sonra yeniden baglaniyor...')
-    setTimeout(createBot, 10000)
-  })
+bot.on('error', (err) => {
+  console.log('Hata:', err.message)
+})
 
-  bot.on('error', (err) => {
-    console.log('Hata:', err.message)
-  })
-
-  bot.on('kicked', (reason) => {
-    console.log('Atildi:', reason)
-  })
-}
-
-createBot()
+bot.on('kicked', (reason) => {
+  console.log('Atildi:', reason)
+})
