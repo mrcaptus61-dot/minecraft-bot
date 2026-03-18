5326474c12c188f8f75521e7ce1967174024a852
mrcaptus61-dot <mrcaptus61@gmail.com>
2026-03-18 22:13:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const bot = mineflayer.createBot({
   host: 'samsun5361.aternos.me',
   port: 21808,
   username: 'AfkBot_Mert',
-  version: '1.21.1',
+  version: false,
   checkTimeoutInterval: 60000
 })
 
 bot.on('spawn', () => {
-  console.log('Bot sunucuya baglandi!')
+  console.log('Bot sunucuya baglandi! Versiyon:', bot.version)
 })
 
 bot.on('end', (reason) => {
   console.log('Baglanti kesildi:', reason)
-  // GitHub Actions timeout ile yeniden tetiklenecek
 })
 
 bot.on('error', (err) => {
