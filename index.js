fbf915153bd80572f582d7d5adaf3c2274d1c6f6
mrcaptus61-dot <mrcaptus61@gmail.com>
2026-03-18 22:03:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,21 +11,28 @@ function createBot() {
     host: 'samsun5361.aternos.me',
     port: 21808,
     username: 'AfkBot_Mert',
-    version: '1.21.11'
+    version: '1.21.1',
+    checkTimeoutInterval: 60000,
+    connect: (client) => {
+      client.socket.setKeepAlive(true, 4000)
+    }
   })
 
   bot.on('spawn', () => {
     console.log('Bot sunucuya baglandi!')
   })
 
-  bot.on('end', () => {
-    console.log('Baglanti kesildi, 5 saniye sonra yeniden baglaniyor...')
-    setTimeout(createBot, 5000)
+  bot.on('end', (reason) => {
+    console.log('Baglanti kesildi (' + reason + '), 10 saniye sonra yeniden baglaniyor...')
+    setTimeout(createBot, 10000)
   })
 
   bot.on('error', (err) => {
     console.log('Hata:', err.message)
-    setTimeout(createBot, 5000)
+  })
+
+  bot.on('kicked', (reason) => {
+    console.log('Atildi:', reason)
   })
 }
 
